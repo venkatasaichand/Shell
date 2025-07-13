9ac98a0b7c8afed39bce77e11144eb175ed29479
root <root@ip-10-7-7-117.ec2.internal>
2025-07-13 11:32:29 +0000

    ok commit

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -8,7 +8,8 @@
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
-#
+
+#ok
 #
 # Please provide your github token and rest api to the script as input
 ################################
